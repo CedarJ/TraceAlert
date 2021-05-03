ee149e8a3d73c41f4afdff6fc1ec4449ce1f1203
Yumeng Zhang <yumeng.zhang01@student.adelaide.edu.au>
2021-05-03 15:15:05 +0800

    Milestone 1 completed

--- TraceAlert/client/www/js/bluetoothlocation.js
+++ TraceAlert/client/www/js/bluetoothlocation.js
@@ -19,7 +19,6 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-//document.addEventListener('deviceready', onDeviceReady, false);
 document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
 
